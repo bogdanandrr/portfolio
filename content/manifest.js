4453ce3a66faa00bc3805fb9f74fde560fbfb5aa
bogdanandrr <ciobanu.andreibogdan97@gmail.com>
2026-07-23 00:39:17 +0300

    modificari

---
+++ content/manifest.js
@@ -0,0 +1,1 @@
+window.PORTFOLIO_FILES = ["0001.mp4","001_1.mp4","01_1.mp4","02.mp4","10_1.mp4","11.mp4","12.mp4","1_1.mp4","3_1.mp4","5.mp4","6 cinematic_1.mp4","6.mp4","7.mp4","8.mp4","Comp 1_1.mp4"];
